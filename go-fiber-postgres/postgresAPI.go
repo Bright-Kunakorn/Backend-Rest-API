8e52f933c90a123363a254384c3b4f30504b3794
kunakorn topurin <97731297+Bright-Kunakorn@users.noreply.github.com>
2023-04-10 15:46:39 +0700

    get api

--- go-fiber-postgres/postgresAPI.go
+++ go-fiber-postgres/postgresAPI.go
@@ -2,12 +2,35 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
+	"net/http"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
+
+type SKU struct {
+	SKUID           string    `json:"skuid"`
+	BarcodePOS      string    `json:"barcodepos"`
+	ProductName     string    `json:"productname"`
+	BrandID         int       `json:"brandid"`
+	ProductGroupID  int       `json:"productgroupid"`
+	ProductCatID    int       `json:"productcatid"`
+	ProductSubCatID int       `json:"productsubcatid"`
+	ProductSizeID   int       `json:"productsizeid"`
+	ProductUnit     int       `json:"productunit"`
+	PackSize        string    `json:"packsize"`
+	Unit            int       `json:"unit"`
+	BanForPracharat int       `json:"banforpracharat"`
+	IsVat           bool      `json:"isvat"`
+	CreateBy        string    `json:"createby"`
+	CreateDate      time.Time `json:"createdate"`
+	IsActive        bool      `json:"isactive"`
+	MerchantID      string    `json:"merchantid"`
+	MapSKU          string    `json:"mapsku"`
+	IsFixPrice      bool      `json:"isfixprice"`
+}
 
 func main() {
 	connStr := "postgresql://root:secret@localhost:5433?sslmode=disable"
@@ -21,44 +44,37 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Successfully connected to PostgreSQL database!")
+	log.Println("Successfully connected to PostgreSQL database!")
 
-	rows, err := db.Query("SELECT * FROM backendposdatasku")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
+	r := gin.Default()
 
-	var skuid string
-	var barcodepos string
-	var productname string
-	var brandid int
-	var productgroupid int
-	var productcatid int
-	var productsubcatid int
-	var productsizeid int
-	var productunit int
-	var packsize string
-	var unit int
-	var banforpracharat int
-	var isvat bool
-	var createby string
-	var createdate time.Time
-	var isactive bool
-	var merchantid string
-	var mapsku string
-	var isfixprice bool
-
-	for rows.Next() {
-		err := rows.Scan(&skuid, &barcodepos, &productname, &brandid, &productgroupid, &productcatid, &productsubcatid, &productsizeid, &productunit, &packsize, &unit, &banforpracharat, &isvat, &createby, &createdate, &isactive, &merchantid, &mapsku, &isfixprice)
+	r.GET("/skus", func(c *gin.Context) {
+		rows, err := db.Query("SELECT * FROM backendposdatasku")
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("%s | %s | %s | %d | %d | %d | %d | %d | %d | %s | %d | %d | %t | %s | %v | %t | %s | %s | %t\n", skuid, barcodepos, productname, brandid, productgroupid, productcatid, productsubcatid, productsizeid, productunit, packsize, unit, banforpracharat, isvat, createby, createdate, isactive, merchantid, mapsku, isfixprice)
-	}
+		defer rows.Close()
 
-	err = rows.Err()
-	if err != nil {
+		var skus []SKU
+
+		for rows.Next() {
+			var sku SKU
+			err := rows.Scan(&sku.SKUID, &sku.BarcodePOS, &sku.ProductName, &sku.BrandID, &sku.ProductGroupID, &sku.ProductCatID, &sku.ProductSubCatID, &sku.ProductSizeID, &sku.ProductUnit, &sku.PackSize, &sku.Unit, &sku.BanForPracharat, &sku.IsVat, &sku.CreateBy, &sku.CreateDate, &sku.IsActive, &sku.MerchantID, &sku.MapSKU, &sku.IsFixPrice)
+			if err != nil {
+				log.Fatal(err)
+			}
+			skus = append(skus, sku)
+		}
+
+		err = rows.Err()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		c.JSON(http.StatusOK, skus)
+	})
+
+	if err := r.Run(":8080"); err != nil {
 		log.Fatal(err)
 	}
 }
